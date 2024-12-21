cd9fe876f4bc32cde28be6a384b43152b0febd66
thetimiiii <shopifysalesai@gmail.com>
2024-12-21 19:04:07 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-            </Routes>
-        </Router>
-    );
-};
-
-export default App;
+createRoot(document.getElementById("root")!).render(<App />);
