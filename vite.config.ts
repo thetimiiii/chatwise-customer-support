97a1b8bf4097c940d2b50451bf38f714a8e21be2
thetimiiii <shopifysalesai@gmail.com>
2024-12-23 17:42:02 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
@@ -10,6 +10,7 @@ export default defineConfig({
     },
   },
   build: {
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: undefined,
